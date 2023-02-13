a584ce253f6cec40dd87358a0ae4501c154a9067
Ninja <cuansenputra@gmail.com>
2023-02-13 11:01:27 +0700

    making post req

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 //cof path
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 //route
 app.get('/products',  async (req, res) => {
@@ -28,6 +29,21 @@ app.get('/products',  async (req, res) => {
     res.render('products/index', { products })
     })
 
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})  
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product })
+})
 //cof port
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000")
